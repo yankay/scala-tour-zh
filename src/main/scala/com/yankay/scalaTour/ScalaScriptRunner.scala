93eeaadfa213ad6a2a5ab98e9e4c9a82b6409fe5
YanKay <yankaycom@gmail.com>
2013-05-02 21:06:37 +0800

    Update

--- src/main/scala/com/yankay/scalaTour/ScalaScriptRunner.scala
+++ src/main/scala/com/yankay/scalaTour/ScalaScriptRunner.scala
@@ -51,13 +51,14 @@ object ScalaScriptCompiler {
   def compile(script: String, err: OutputStream): Option[File] = {
     val scriptFile = File.makeTemp("scala-script", ".scala")
     // save the command to the file
-    scriptFile writeAll """import scala.io.Codec
+    val scriptheader =  """import scala.io.Codec
     import java.nio.charset.CodingErrorAction
+
     implicit val codec = Codec("UTF-8")
     codec.onMalformedInput(CodingErrorAction.REPLACE)
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
     """
-    scriptFile writeAll script
+    scriptFile.writeAll(scriptheader+script)
     try compile(scriptFile, err)
     finally scriptFile.delete() // in case there was a compilation error
   }
